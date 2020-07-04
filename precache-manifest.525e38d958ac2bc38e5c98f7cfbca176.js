d6b83ecb68eb6152f3c80cb8a441d4fc582518a2
Marlon Wiprud <marlonwiprud1@gmail.com>
2020-07-04 11:36:00 -0400

    Updates

---
+++ precache-manifest.525e38d958ac2bc38e5c98f7cfbca176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4909e20c6eb19547cab5e1ceb5d1a61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bd9af8a77009194c830",
+    "url": "/static/css/main.d7e8962b.chunk.css"
+  },
+  {
+    "revision": "25b80a4ddb791427b243",
+    "url": "/static/js/2.6f5ecc2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f5ecc2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd9af8a77009194c830",
+    "url": "/static/js/main.a358bd35.chunk.js"
+  },
+  {
+    "revision": "f2563fd789f539a2d78c",
+    "url": "/static/js/runtime-main.41b2dece.js"
+  }
+]);
